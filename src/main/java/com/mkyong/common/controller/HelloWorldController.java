2935886be828bbfd4b35d0ade0cab17ea70c9e29
proyectos <proyectos@PCPRY01>
2017-01-12 12:00:54 -0500

    update 2

--- src/main/java/com/mkyong/common/controller/HelloWorldController.java
+++ src/main/java/com/mkyong/common/controller/HelloWorldController.java
@@ -16,7 +16,7 @@ public class HelloWorldController extends AbstractController {
 			HttpServletResponse response) throws Exception {
 
 		ModelAndView model = new ModelAndView("HelloWorldPage");
-		model.addObject("msg", "hello world");
+		model.addObject("msg", "demo controller");
 
 		return model;
 	}
