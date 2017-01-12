dcca6eebafeaa8dd18cc9487bd885c9474f01283
proyectos <proyectos@PCPRY01>
2017-01-12 16:47:26 -0500

    ACCESO AUTORIWED

--- src/main/java/com/mkyong/common/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/mkyong/common/service/impl/UsuarioServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.mkyong.common.dao.UsuarioDao;
 import com.mkyong.common.domain.Usuario;
